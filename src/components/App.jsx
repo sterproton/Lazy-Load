b0519c6230d07676179efb59b4981191465d4e16
Sterchois <sterchois@protonmail.com>
2018-10-29 23:16:39 +0800

    add react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import ArticleContainer from './ArticleContainer/ArticleContainer'
 import Header from './Header/Header'
 import '../assets/scss/global.scss'
 import '../assets/scss/index.scss'
 
-
 class App extends Component {
   render() {
     return (
-      <div className="reactAPP">
-        <Header />
-        <ArticleContainer />
-      </div>
+      <Router>
+        <div className="container">
+          <Header />
+          <ArticleContainer />
+        </div>
+        
+      </Router>
     )
   }
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Block from './Block'
 import './Header.scss'
 
@@ -8,7 +9,8 @@ const Header = () => {
     <div className="header">
       <div className="header-img"><img src="https://news.ycombinator.com/favicon.ico" alt="" /></div>
       <div className="block-wrap">
-        {blocks.map(content => <Block key={content} content={content} />)}
+        {/* {blocks.map(content => <Block key={content} content={content} />)} */}
+        {blocks.map(content => <Link to={`/${content}`}>{content}</Link>)}
       </div>
     </div>
   )
